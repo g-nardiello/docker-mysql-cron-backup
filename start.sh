d39f664b960842e16da93571fc8da12403805803
g.nardiello <giuseppe@nards.it>
2022-12-21 20:14:28 +0100

    Trap with function on start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,13 @@
+function final_backup {
+    DATE=$(date +%Y%m%d%H%M)
+    echo "=> Requested last backup at $(date "+%Y-%m-%d %H:%M:%S")"
+    exec /backup
+    exit 0
+}
+
 if ! [ -z "${EXIT_BACKUP}" ]; then
   echo "=> Listening on container shutdown to make last backup"
-  trap "/backup.sh" SIGHUP SIGINT SIGTERM
+  trap final_backup SIGHUP SIGINT SIGTERM
 fi
 
-dockerize -wait tcp://${MYSQL_HOST}:${MYSQL_PORT} -timeout ${TIMEOUT} /run.sh
-
-while : ; do sleep 1 ; done
+exec dockerize -wait tcp://${MYSQL_HOST}:${MYSQL_PORT} -timeout ${TIMEOUT} /run.sh
